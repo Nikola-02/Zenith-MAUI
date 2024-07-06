23262654e159f6822d07b3577bb3532988b0fb7d
Nikola-02 <nikoladjunisic@gmail.com>
2024-07-06 14:24:30 +0200

    Finished delete for playlist

--- Zenith-MAUI/Components/PlaylistComponent.xaml.cs
+++ Zenith-MAUI/Components/PlaylistComponent.xaml.cs
@@ -1,5 +1,7 @@
+using RestSharp;
 using System.Diagnostics;
 using System.Xml.Linq;
+using Zenith_MAUI.Common;
 using Zenith_MAUI.Pages;
 
 namespace Zenith_MAUI.Components;
@@ -18,7 +20,7 @@ public partial class PlaylistComponent : ContentView
     public static BindableProperty TracksCountProperty =
        BindableProperty.Create(nameof(TracksCount), typeof(int), typeof(PlaylistComponent), 0, BindingMode.OneWay);
 
-
+    public MProp<string> Error { get; set; } = new MProp<string>();
 
     public PlaylistComponent()
 	{
@@ -49,13 +51,31 @@ public partial class PlaylistComponent : ContentView
         set => SetValue(TracksCountProperty, value);
     }
 
-    private void Button_Clicked(object sender, EventArgs e)
-    {
-
-    }
-
     private void Button_Clicked_1(object sender, EventArgs e)
     {
         App.Current.MainPage.Navigation.PushModalAsync(new EditPlaylistPage(Id, Name));
+    }
+
+    private void Button_Clicked_2(object sender, EventArgs e)
+    {
+        try
+        {
+            RestRequest request = new RestRequest("playlists/" + Id);
+
+            var response = Api.Client.Delete(request);
+
+            if (!response.IsSuccessful)
+            {
+                Error.Error = response.ErrorMessage;
+            }
+            else
+            {
+                App.Current.MainPage = new NavigationPage(new Playlists());
+            }
+        }
+        catch (Exception ex)
+        {
+            Error.Error = "Desila se greska";
+        }
     }
 }

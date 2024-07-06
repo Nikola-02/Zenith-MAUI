62d48140d1fbf77ec5075d9a4610c2cf9122cfe1
Nikola <nikoladjunisic@gmail.com>
2024-07-06 11:22:06 +0200

    Added Validation for playlist

--- Zenith-MAUI/Pages/AddPlaylistPage.xaml.cs
+++ Zenith-MAUI/Pages/AddPlaylistPage.xaml.cs
@@ -1,13 +1,22 @@
+using FluentValidation.Results;
+using RestSharp;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Zenith_MAUI.Common;
+using Zenith_MAUI.Validators;
 
 namespace Zenith_MAUI.Pages;
 
 public partial class AddPlaylistPage : ContentPage
 {
-	public AddPlaylistPage()
+    public MProp<string> Name { get; set; } = new MProp<string>();
+    public MProp<string> Error { get; set; } = new MProp<string>();
+
+    public AddPlaylistPage()
 	{
-		InitializeComponent();
+        Name.OnChange = Validate;
+
+        InitializeComponent();
 	}
 
     private void Button_Clicked(object sender, EventArgs e)
@@ -15,9 +24,48 @@ public partial class AddPlaylistPage : ContentPage
         App.Current.MainPage.Navigation.PopModalAsync();
     }
 
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        var name = Name.Value;
+
+        RestRequest request = new RestRequest("playlists");
+
+        request.AddJsonBody(new { name });
+
+        var response = Api.Client.Post(request);
+
+        if (response.IsSuccessful)
+        {
+            App.Current.MainPage.Navigation.PopModalAsync();
+            //App.Current.MainPage.Navigation.PushAsync(new Playlists());
+        }
+        else
+        {
+           Error.Value = response.ErrorMessage;
+        }
+    }
+
+    private void Validate()
+    {
+        MAddEditPlaylistValidator validator = new MAddEditPlaylistValidator();
+
+        ValidationResult result = validator.Validate(this);
+
+        if (!result.IsValid)
+        {
+            Name.Error = result.GetError("Name");
+        }
+        else
+        {
+            Name.Error = null;
+        }
+    }
+
     public event PropertyChangedEventHandler? PropertyChanged;
     protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
+
+    
 }
---
+++ Zenith-MAUI/Validators/MAddEditPlaylistValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenith_MAUI.Pages;
+
+namespace Zenith_MAUI.Validators
+{
+    public class MAddEditPlaylistValidator : AbstractValidator<AddPlaylistPage>
+    {
+        public MAddEditPlaylistValidator()
+        {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(x => x.Name.Value)
+                .NotEmpty()
+                .WithMessage("Naziv je obavezan.");
+        }
+    }
+}
